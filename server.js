3fc7b191d03206bf01577cd246e070bfe09d69d9
chamano sidney <chamano.sr@gmail.com>
2020-10-22 09:31:31 +0200

    setting route for store

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ console.log(stripeSecretKey)
 
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/store', (req,res) =>{
+    fs.readFile('Items.json', (error,)=>{
+        if(error){
+            res.status(500).end()
+        }else{
+            res.render('store.ejs',{
+                items:JSON.parse(data)
+            })
+        }
+    })
+})
 
 app.listen(3000, ()=>{
     console.log('Server has started')
