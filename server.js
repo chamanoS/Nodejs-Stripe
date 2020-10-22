1ab4f8ab7244186f1731d600d43aa8b76059259f
chamano sidney <chamano.sr@gmail.com>
2020-10-22 09:23:00 +0200

    setting up stripe

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+console.log(stripePublicKey)
+console.log(stripeSecretKey)
+
 const express = require('express')
 const app = express()
 
